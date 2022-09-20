47f6c57b0afd90d495ca94d74c7cd14636ad72dc
Merlyn Tickler <mtickler@si-c6.nsk.21-school.ru>
2022-09-20 13:27:37 +0700

    Feat: add memcmp implementation

--- src/memcmp.c
+++ src/memcmp.c
@@ -1,4 +1,13 @@
 #include "libft.h"
 
 int ft_memcmp(const void *s1, const void *s2, size_t n) {
+    if (s1 != s2) {
+        const unsigned char* str1 = (const unsigned char*) s1;
+        const unsigned char* str2 = (const unsigned char*) s2;
+        for (size_t i = 0; i != n; ++i) {
+            if (str1[i] != str2[i])
+                return str1[i] - str2[i];
+        }
+    }
+    return 0;
 }
