14d0800e6ca61f23c8997b239c2df7bcdb0ec3cb
roshik14 <mtickler@student.21-school.ru>
2022-09-21 22:16:12 +0700

    Fix: reimplemented putnbr_fd function

--- src/put_fd.c
+++ src/put_fd.c
@@ -11,10 +11,34 @@ void ft_putstr_fd(char *s, int fd) {
 
 void ft_putendl_fd(char *s, int fd) {
     ft_putstr_fd(s, fd);
-    write(fd, "\n", 1);
+    write(fd, "\n", sizeof(char));
+}
+
+static size_t get_num_len(long n) {
+    size_t len = 1;
+    while (n != 0) {
+        len *= 10;
+        n /= 10;
+    }
+    return len / 10;
 }
 
 void ft_putnbr_fd(int n, int fd) {
-    char *n_str = ft_itoa(n);
-    write(fd, n_str, ft_strlen(n_str));
+    char minus = '-';
+    if (!n) {
+        char c = n + '0';
+        write(fd, &c, sizeof(char));
+    }
+    long num = n;
+    if (num < 0) {
+        write(1, &minus, sizeof(char));
+        num *= -1l;
+    }
+    int len = get_num_len(num);
+    while (num != 0) {
+        char c = num / len + '0';
+        write(fd, &c, sizeof(char));
+        num %= len;
+        len /= 10;
+    }
 }
