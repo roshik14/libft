6f9b955fe73b8862a79f846cc811ed77c0e1941a
mtickler Ubuntu 21.10 <mtickler@student.21-school.ru>
2022-09-21 21:29:03 +0700

    Fix: find nul termanate char

--- src/strchr.c
+++ src/strchr.c
@@ -2,11 +2,12 @@
 
 char *ft_strchr(const char *s, int c) {
     char *str = (char*) s;
-    while(*str) {
-        if (*str == c) {
+    while (*str) {
+        if (*str == c)
             return str;
-        }
-        s += 1;
+        str += 1;
     }
+    if (*str == c)
+        return str;
     return NULL;
 }
