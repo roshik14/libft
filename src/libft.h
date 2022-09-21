cfecb56810a2aff3dbe0d1beb9435bbbc47fb23c
mtickler Ubuntu 21.10 <mtickler@student.21-school.ru>
2022-09-21 21:24:12 +0700

    Feat: isspace function

--- src/libft.h
+++ src/libft.h
@@ -3,6 +3,23 @@
 
 #include <stddef.h>
 
+#ifdef BONUS
+typedef struct s_list {
+    void *content;
+    struct s_list *next;
+} t_list;
+
+t_list *ft_lstnew(void *content);
+void ft_lsadd_front(t_list **lst, t_list *new_lst);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
+void ft_lstadd_back(t_list **lst, t_list *new_lst);
+void ft_lstdelone(t_list *lst, void (*del)(void*));
+void ft_lstclear(t_list **lst, void (*del)(void*));
+void ft_lstiter(t_list *lst, void (*f)(void *));
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+#endif 
+
 void *ft_memset(void *s, int c, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
@@ -22,6 +39,7 @@ int ft_isalpha(int c);
 int ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
+int ft_isspace(int c);
 int ft_toupper(int c);
 int ft_tolower(int c);
 void *ft_calloc(size_t nmemb, size_t size);
