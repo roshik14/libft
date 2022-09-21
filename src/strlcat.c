54902e54bdb12ad9791dc8f9b6f77bd9b52d8910
mtickler Ubuntu 21.10 <mtickler@student.21-school.ru>
2022-09-21 21:31:21 +0700

    Fix: reimplemented

--- src/strlcat.c
+++ src/strlcat.c
@@ -1,10 +1,19 @@
 #include "libft.h"
+#include <stdio.h>
 
 size_t ft_strlcat (char *dst, const char *src, size_t size) {
-    size_t start = ft_strlen(dst);
-    for (size_t i = 0, j = start; i != size - 1; ++i, ++j) {
-        dst[j++] = src[i];
+    if ((!dst && !src) || !size)
+        return size;
+    size_t dst_len = ft_strlen(dst);
+    size_t src_len = ft_strlen(src);
+    if (size < dst_len) {
+        return src_len + size;
     }
-    dst[size - 1] = '\0';
-    return start + ft_strlen(src);
+    if (size > src_len)
+        size = src_len;
+    for (size_t i = 0, j = dst_len; src[i] && i != size; ++i, ++j) {
+        dst[j] = src[i];
+    }
+    dst[!src_len ? dst_len : dst_len + size] = '\0';
+    return dst_len + src_len;
 }
