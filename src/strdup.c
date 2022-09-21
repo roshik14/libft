5030f1de679f25cc33e3293b1baab3a99b0581ac
mtickler Ubuntu 21.10 <mtickler@student.21-school.ru>
2022-09-21 21:29:54 +0700

    Fix: mv strlcpy to memcpy and add nul terminate char

--- src/strdup.c
+++ src/strdup.c
@@ -1,9 +1,11 @@
 #include "libft.h"
-#include <stdlib.h>
 
 char *ft_strdup(const char *s) {
-    size_t len = ft_strlen(s);
-    char *str = (char*) malloc(len);
-    if (str != NULL) ft_strlcpy(str, s, len);
+    size_t len = ft_strlen(s) + 1;
+    char *str = ft_calloc(len, sizeof(char));
+    if (str != NULL) {
+        ft_memcpy(str, s, len);
+        str[len] = '\0';
+    }
     return str;
 }
