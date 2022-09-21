61a0f374983a8cf79ff07482f18e72c8c421d345
mtickler Ubuntu 21.10 <mtickler@student.21-school.ru>
2022-09-21 21:30:31 +0700

    Fix: another algorithm

--- src/strncmp.c
+++ src/strncmp.c
@@ -2,10 +2,11 @@
 
 int ft_strncmp(const char *s1, const char *s2, size_t n) {
     if (s1 != s2) {
+        const unsigned char *str1 = (const unsigned char*) s1;
+        const unsigned char *str2 = (const unsigned char*) s2;
         for (size_t i = 0; i != n; ++i) {
-            if (s1[i] != s2[i]) {
-                return s1[i] - s2[i];
-            }
+            if (!str1[i] || !str2[i] || str1[i] != str2[i])
+                return (int)(str1[i] - str2[i]);
         }
     }
     return 0;
