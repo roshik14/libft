d7b66a994a3033439d1783b7d45745b9851185d9
roshik14 <mtickler@student.21-school.ru>
2022-09-22 07:59:33 +0700

    Fix: done

--- src/substr.c
+++ src/substr.c
@@ -1,13 +1,17 @@
 #include "libft.h"
 
 char *ft_substr(char const *s, unsigned int start, size_t len) {
-    char *sub = (char*) ft_calloc(len, sizeof(char));
-    if (!sub)
-        return NULL;
-    unsigned s_len = ft_strlen(s);
-    for (size_t i = start, j = 0; i != s_len && j != len - 1; ++i, ++j) {
-        sub[j] = s[i];
+    if (start >= len || !s || !len) {
+        char *sub = (char*)ft_calloc(1, sizeof(char));
+        return sub ? sub : NULL;
     }
-    sub[len - 1] = '\0';
+    char *sub = (char*) ft_calloc(len+1, sizeof(char));
+    if (sub) {
+        unsigned s_len = ft_strlen(s);
+        for (size_t i = start, j = 0; i != s_len && j != len; ++i, ++j) {
+            sub[j] = s[i];
+        }
+        sub[len] = '\0';
+    }
     return sub;
 }
