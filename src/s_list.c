9e96ccfd5299640416f7c9fbe68fda5aeef00471
Merlyn Tickler <mtickler@pr-d10.nsk.21-school.ru>
2022-09-22 12:29:58 +0700

    Feat: try to make

--- src/s_list.c
+++ src/s_list.c
@@ -3,26 +3,39 @@
 
 t_list *ft_lstnew(void *content) {
     t_list * lst = ft_calloc(1, sizeof(t_list));
-    lst->content = content;
-    lst->next = NULL;
+    if (lst) {
+        lst->content = content;
+        lst->next = NULL;
+    }
+    return lst;
 }
 
 void ft_lstadd_front(t_list **lst, t_list *new_lst) {
+    new_lst->next = *lst;
+    *lst = new_lst;
 }
 
+void ft_lstadd_back(t_list **lst, t_list *new_lst) {
+    t_list *cur = *lst;
+    while (cur) {
+        cur = cur->next;
+    }
+    new_lst->next = cur;
+    cur = new_lst;
+}
 
 int ft_lstsize(t_list *lst) {
     int len = 0;
-    while (lst->next) {
+    while (lst) {
         ++len;
-        lst->next = lst;
+        lst = lst->next;
     }
     return len;
 }
 
 t_list *ft_lstlast(t_list *lst) {
-    while (lst->next) {
-        lst->next = lst;
+    while (lst) {
+        lst = lst->next;
     }
     return lst->content;
 }
@@ -34,10 +47,32 @@ void ft_lstdelone(t_list *lst, void (*del)(void*)) {
 
 void ft_lstclear(t_list **lst, void (*del)(void*)) {
     t_list *l = *lst;
-    while (l->next) {
+    while (l) {
         del(l->content);
-        l->next = l;
-        free(l);
+        l = l->next;
     }
     lst = NULL;
+}
+
+void ft_lstiter(t_list *lst, void (*f)(void *)) {
+    while (lst) {
+        f(lst->content);
+        lst = lst->next;
+    }
+}
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *)) {
+    t_list *res = NULL;
+    while (lst) {
+        res = ft_lstnew(f(lst->current));
+        if (!res)
+            return NULL;
+        lst = lst->next;
+        if (lst) {
+            t_list *next = ft_lstnew(lst->next);
+            res->next = next;
+            res = res->next;
+        }
+    }
+    return res;
 }
