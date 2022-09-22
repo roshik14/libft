42f6eaee71766c7c775ddf22f6406b7c77285367
roshik14 <fraust1337@gmail.com>
2022-09-22 23:35:59 +0700

    Fix: delete ifdef

--- src/libft.h
+++ src/libft.h
@@ -3,7 +3,6 @@
 
 #include <stddef.h>
 
-#ifdef BONUS
 typedef struct s_list {
     void *content;
     struct s_list *next;
@@ -18,7 +17,6 @@ void ft_lstdelone(t_list *lst, void (*del)(void*));
 void ft_lstclear(t_list **lst, void (*del)(void*));
 void ft_lstiter(t_list *lst, void (*f)(void *));
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-#endif 
 
 void *ft_memset(void *s, int c, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
