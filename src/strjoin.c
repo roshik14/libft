a8bcab2450e42304340376ecc94342e21fbfedd9
roshik14 <mtickler@student.21-school.ru>
2022-09-22 07:59:23 +0700

    Fix: done

--- src/strjoin.c
+++ src/strjoin.c
@@ -3,11 +3,14 @@
 char *ft_strjoin(char const *s1, char const *s2) {
     size_t first_len = ft_strlen(s1);
     size_t second_len = ft_strlen(s2);
-    char *res = ft_calloc(first_len + second_len, sizeof(char));
+    char *res = ft_calloc(first_len + second_len + 2, sizeof(char));
     if (!res)
         return NULL;
-    ft_strlcat(res, s1, first_len);
-    ft_strlcat(res, s2, second_len);
+    ft_memcpy(res, s1, first_len);
+    for (size_t i = first_len, j = 0; j != second_len; ++j, ++i) {
+        res[i] = s2[j];
+    }
     return res;
 }
+
 
