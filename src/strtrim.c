b546de34e26c668922fc20e710b5dde1f9d66370
roshik14 <mtickler@student.21-school.ru>
2022-09-22 23:20:46 +0700

    Fix: done

--- src/strtrim.c
+++ src/strtrim.c
@@ -1,17 +1,18 @@
 #include "libft.h"
-#include <stdio.h>
 
 static size_t count_substr_len(char const *start, char const *end) {
     size_t len = 0;
     while (start != end) {
         ++len;
-        ++start;
+        start += 1;
     }
     return len;
 }
 
 static char* create_empty_str() {
     char *sub = ft_calloc(1, sizeof(char));
+    if (!sub)
+        return NULL;
     return sub;
 }
 
@@ -29,28 +30,28 @@ static char const* skip_from_right(char const *s1, char const *set) {
 }
 
 static char* get_substr(char const *s1, char const *end) {
-    char *substr = ft_calloc(count_substr_len(s1, end), sizeof(char));
+    char *substr = ft_calloc(count_substr_len(s1, end) + 2, sizeof(char));
+    if (!substr)
+        return NULL;
     size_t i = 0;
     while (s1 != end) {
         substr[i++] = *s1;
         s1 += 1;
     }
     substr[i] = *end;
+    substr[i+1] = '\0';
     return substr;
 }
 
 char *ft_strtrim(char const *s1, char const *set) {
     if (!s1 || !set)
         return NULL;
-
-    if (!ft_strlen(s1) || !ft_strlen(set)) {
-        return create_empty_str();
-    }
-
-    s1 = skip_from_left(s1, set);
-    if (ft_strlen(s1)) {
-        char const *end = skip_from_right(s1, set);
-        return get_substr(s1, end);
+    if (ft_strlen(s1) && ft_strlen(set)) {
+        s1 = skip_from_left(s1, set);
+        if (ft_strlen(s1)) {
+            char const *end = skip_from_right(s1, set);
+            return get_substr(s1, end);
+        }
     }
     return create_empty_str();
 }
