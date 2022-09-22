de8c5523b314de768f08b5e74ea78b028ab58bf7
roshik14 <mtickler@student.21-school.ru>
2022-09-22 23:20:05 +0700

    Fix: done

--- src/itoa.c
+++ src/itoa.c
@@ -1,8 +1,10 @@
 #include "libft.h"
-static size_t get_number_len(int n) {
+
+static size_t get_number_len(long n) {
     size_t len = 0;
     if (n < 0) {
         n *= -1;
+        ++len;
     }
     while (n != 0) {
         ++len;
@@ -11,20 +13,42 @@ static size_t get_number_len(int n) {
     return len;
 }
 
-char *ft_itoa(int n) {
-    size_t num_len = get_number_len(n);
-    char *str = ft_calloc(n < 0 ? num_len + 1 : num_len, sizeof(char));
-    if (!str)
-        return NULL;
-    int end = 0;
-    if (n < 0) {
-        str[0] = '-';
-        n *= -1;
-        end = 1;
-    }
-    for (int i = num_len - 1; i >= end; --i) {
-        str[i] = n % 10;
-        n /= 10;
+static char *get_zero_num() {
+    char *str = ft_calloc(2, sizeof(char));
+    if (str) {
+        str[0] = '0';
+        str[1] = '\0';
     }
     return str;
+}
+
+static void fill_str(char *str, long n, int num_len, int end) {
+    for (int i = num_len - 1; i >= end; --i) {
+        str[i] = n % 10 + '0';
+        n /= 10;
+    }
+    str[num_len] = '\0';
+}
+
+static char* int_to_str(long n, int num_len) {
+    char *str = ft_calloc(num_len + 1, sizeof(char));
+    if (str) {
+        int end = 0;
+        if (n < 0) {
+            str[0] = '-';
+            n *= -1;
+            end = 1;
+        }
+        fill_str(str, n, num_len, end);
+    }
+    return str;
+}
+
+char *ft_itoa(int n) {
+    long num = n;
+    size_t num_len = get_number_len(num);
+    if (num_len) {
+        return int_to_str(num, num_len);
+    }
+    return get_zero_num();
 }
