72be390132f039958bcc666e938ff24c71fff91f
roshik14 <fraust1337@gmail.com>
2022-09-23 22:44:26 +0700

    Fix: done

--- src/s_list.c
+++ src/s_list.c
@@ -1,4 +1,6 @@
 #include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 t_list *ft_lstnew(void *content) {
     t_list * lst = ft_calloc(1, sizeof(t_list));
@@ -15,12 +17,14 @@ void ft_lstadd_front(t_list **lst, t_list *new_lst) {
 }
 
 void ft_lstadd_back(t_list **lst, t_list *new_lst) {
-    t_list *cur = *lst;
-    while (cur) {
-        cur = cur->next;
+    if (*lst) {
+        t_list *cur = *lst;
+        while (cur->next)
+            cur = cur->next;
+        cur->next = new_lst;
+    } else {
+        *lst = new_lst;
     }
-    new_lst->next = cur;
-    cur = new_lst;
 }
 
 int ft_lstsize(t_list *lst) {
@@ -33,10 +37,9 @@ int ft_lstsize(t_list *lst) {
 }
 
 t_list *ft_lstlast(t_list *lst) {
-    while (lst) {
+    while (lst && lst->next)
         lst = lst->next;
-    }
-    return lst->content;
+    return lst;
 }
 
 
@@ -45,12 +48,18 @@ void ft_lstdelone(t_list *lst, void (*del)(void*)) {
 }
 
 void ft_lstclear(t_list **lst, void (*del)(void*)) {
-    t_list *l = *lst;
-    while (l) {
+    if (*lst) {
+        t_list *l = *lst;
+        while (l->next) {
+            t_list *tmp = l->next;
+            del(l->content);
+            free(l);
+            l = tmp;
+        }
         del(l->content);
-        l = l->next;
+        free(l);
+        *lst = NULL;
     }
-    lst = NULL;
 }
 
 void ft_lstiter(t_list *lst, void (*f)(void *)) {
@@ -61,4 +70,18 @@ void ft_lstiter(t_list *lst, void (*f)(void *)) {
 }
 
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *)) {
+    if (lst) {
+        t_list *res = NULL;
+        while (lst) {
+            t_list *n = ft_lstnew(f(lst->content));
+            if (!n) {
+                ft_lstclear(&res, del);
+                return NULL;
+            }
+            ft_lstadd_back(&res, n);
+            lst = lst->next;
+        }
+        return res;
+    }
+    return NULL;
 }
